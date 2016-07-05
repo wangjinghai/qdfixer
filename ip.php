13c103e4c526a55b73f8aa966cc5fc0f4a980974
wjh <65292420@qq.com>
2016-07-05 12:49:09 +0800

    add all files

---
+++ ip.php
@@ -0,0 +1,6 @@
+<?php
+echo $_SERVER["REMOTE_ADDR"];
+echo '<br />';
+echo '<a href="skype:ljmys--1?chat" target="_blank">skype</a><br />';
+echo '<a href="callto://ljmys--1">call</a><br />';
+echo '<a href="tencent://message/?uin=65292420">qq</a>';
